ffddd73cea5054667e639c633bc0d112fff1c8fe
xstefank <xstefank122@gmail.com>
2017-11-28 22:30:58 +0100

    add propeties loading to api-gateway

--- api-gateway/src/main/java/org/learn/lra/apigateway/LRAExecutor.java
+++ api-gateway/src/main/java/org/learn/lra/apigateway/LRAExecutor.java
@@ -1,11 +1,29 @@
 package org.learn.lra.apigateway;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.learn.lra.coreapi.LRA;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
-@Stateless
+//@Stateless
+@Singleton
+@Startup
 public class LRAExecutor {
+
+    @Inject
+    @ConfigProperty(name = "lra.order.url", defaultValue = "order-service")
+    private String orderUrl;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("XXXXXXXXXXXXX test");
+        System.out.println("AAAAAAAAAAAAAAAa " + orderUrl);
+
+    }
 
     public String processLRA(LRA lra) {
         return "Received lra - " + lra;
