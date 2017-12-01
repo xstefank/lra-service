06c8df6b25f9b13c5e2d71525dfd054ac8b2d471
xstefank <xstefank122@gmail.com>
2017-12-01 23:14:26 +0100

    shipment subscription to LRA endpoint

--- service-model/src/main/kotlin/org/learn/lra/coreapi/Domain.kt
+++ service-model/src/main/kotlin/org/learn/lra/coreapi/Domain.kt
@@ -2,5 +2,7 @@ package org.learn.lra.coreapi
 
 data class ProductInfo(val productId: String = "", val comment: String = "", val price: Int = 0)
 
+data class OrderInfo(val orderId: String = "", val product: ProductInfo)
+
 data class ShipmentInfo(val shipmentId: String = "", val shippingPrice: Int = 0)
 
---
+++ shipment-service/src/main/java/org/learn/lra/shipmentservice/rest/ShipmentEndpoint.java
@@ -0,0 +1,38 @@
+package org.learn.lra.shipmentservice.rest;
+
+import io.narayana.lra.annotation.LRA;
+import org.learn.lra.Util;
+import org.learn.lra.coreapi.OrderInfo;
+import org.learn.lra.coreapi.ShipmentInfo;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@ApplicationScoped
+@Path("/")
+public class ShipmentEndpoint {
+
+    private static final String REQUEST = "/request";
+
+    @POST
+    @Path(REQUEST)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @LRA(value = LRA.Type.REQUIRED)
+    public ShipmentInfo requestShipment(OrderInfo orderInfo) {
+        String id = Util.generateId();
+
+        int price = computeShipment(orderInfo);
+
+        return new ShipmentInfo(id, price);
+    }
+
+    private int computeShipment(OrderInfo orderInfo) {
+        //return testing stub
+        return 42;
+    }
+}
