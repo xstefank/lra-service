1706fd0342a8db30c18773c53561fa8d3449e92c
xstefank <xstefank122@gmail.com>
2017-12-02 12:20:24 +0100

    implement LRA endpoints in shipment

--- shipment-service/src/main/java/org/learn/lra/shipmentservice/ShipmentEndpoint.java
+++ shipment-service/src/main/java/org/learn/lra/shipmentservice/ShipmentEndpoint.java
@@ -1,6 +1,10 @@
 package org.learn.lra.shipmentservice;
 
+import io.narayana.lra.annotation.Compensate;
+import io.narayana.lra.annotation.Complete;
 import io.narayana.lra.annotation.LRA;
+import io.narayana.lra.client.LRAClient;
+import org.jboss.logging.Logger;
 import org.learn.lra.Util;
 import org.learn.lra.coreapi.LRAOperationAPI;
 import org.learn.lra.coreapi.OrderInfo;
@@ -9,14 +13,19 @@ import org.learn.lra.coreapi.ShipmentInfo;
 import javax.ejb.EJB;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @ApplicationScoped
 @Path("/")
 public class ShipmentEndpoint {
+
+    private static final Logger log = Logger.getLogger(ShipmentEndpoint.class);
 
     @EJB
     private ShipmentService shipmentService;
@@ -26,12 +35,30 @@ public class ShipmentEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @LRA(value = LRA.Type.REQUIRED)
-    public ShipmentInfo requestShipment(OrderInfo orderInfo) {
-        String id = Util.generateId();
+    public ShipmentInfo requestShipment(@HeaderParam(LRAClient.LRA_HTTP_HEADER) String lraUri, OrderInfo orderInfo) {
+       return shipmentService.computeShipment(orderInfo);
+    }
 
-        int price = shipmentService.computeShipment(orderInfo);
+    @PUT
+    @Path(LRAOperationAPI.COMPLETE)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Complete
+    public Response completeWork(@HeaderParam(LRAClient.LRA_HTTP_HEADER) String lraUri) {
+        String lraId = LRAClient.getLRAId(lraUri);
 
-        return new ShipmentInfo(id, price);
+        shipmentService.persistShipment(lraId);
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Path(LRAOperationAPI.COMPENSATE)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Compensate
+    public Response compensateWork(@HeaderParam(LRAClient.LRA_HTTP_HEADER) String lraUri) {
+        String lraId = LRAClient.getLRAId(lraUri);
+
+        shipmentService.compensateShipment(lraId);
+        return Response.ok().build();
     }
 
 }
--- shipment-service/src/main/java/org/learn/lra/shipmentservice/ShipmentService.java
+++ shipment-service/src/main/java/org/learn/lra/shipmentservice/ShipmentService.java
@@ -1,14 +1,31 @@
 package org.learn.lra.shipmentservice;
 
+import org.jboss.logging.Logger;
+import org.learn.lra.Util;
 import org.learn.lra.coreapi.OrderInfo;
+import org.learn.lra.coreapi.ShipmentInfo;
 
 import javax.ejb.Stateless;
 
 @Stateless
 public class ShipmentService {
 
-    public int computeShipment(OrderInfo orderInfo) {
+    private static final Logger log = Logger.getLogger(ShipmentService.class);
+
+    public ShipmentInfo computeShipment(OrderInfo orderInfo) {
         //return testing stub
-        return 42;
+        String id = Util.generateId();
+
+        return new ShipmentInfo(id , 42);
+    }
+
+    public void persistShipment(String lraId) {
+        log.info("persisting shipment for LRA " + lraId);
+        //TODO actual persist
+    }
+
+    public void compensateShipment(String lraId) {
+        log.info("ocmpensating shipment for LRA " + lraId);
+        //TODO
     }
 }
