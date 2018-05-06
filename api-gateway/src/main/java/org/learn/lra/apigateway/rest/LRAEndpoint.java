10e1ac3233c12fca62c0990a8f650534d45948ea
xstefank <xstefank122@gmail.com>
2018-05-06 10:46:06 +0200

    Code cleanup

--- api-gateway/src/main/java/org/learn/lra/apigateway/rest/LRAEndpoint.java
+++ api-gateway/src/main/java/org/learn/lra/apigateway/rest/LRAEndpoint.java
@@ -6,14 +6,11 @@ import org.learn.lra.apigateway.core.LRAExecutor;
 import org.learn.lra.coreapi.LRADefinition;
 
 import javax.ejb.EJB;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 
 
 @Path("/lra")
@@ -24,13 +21,10 @@ public class LRAEndpoint {
     @EJB
     private LRAExecutor lraExecutor;
 
-    @Context
-    private UriInfo uriInfo;
-
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public String lraRequest(LRADefinition lraDefinition, @Context HttpServletRequest request) {
+    public String lraRequest(LRADefinition lraDefinition) {
         log.info("received lra - " + lraDefinition);
         lraExecutor.processLRA(lraDefinition);
         return "LRA " + lraDefinition.getName() + " is going to be processed";
